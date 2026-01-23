b487f4c242b2c583eec3a64e3791d0bf838eddb2
hooss-only <hoossonly@gmail.com>
2026-01-24 05:18:23 +0900

    Separate initialize_window function from main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
 #include <SDL3/SDL.h>
 
-int main() {
-        SDL_Init(SDL_INIT_VIDEO);
+SDL_Window* window;
+SDL_Renderer* renderer;
+
+int initialize_window() {
+        if (SDL_Init(SDL_INIT_VIDEO)) return 1;
 
         SDL_Window* window = SDL_CreateWindow(
                 "Surviva",
                 800, 600,
                 0
         );
+        if (window == NULL) return 1;
 
         SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+        if (renderer == NULL) return 1;
+
+        return 0;
+}
+
+int main() {
+        if (initialize_window())  return 1;
 
         bool should_close = false;
         
